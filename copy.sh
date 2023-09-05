938035b41721b35db6960abab9a1a7136b3549ea
Stefano Tamagnini <yoghi@sigmalab.net>
2023-09-05 23:52:21 +0200

    fix segnapunti

--- copy.sh
+++ copy.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 mount /mnt/yoghi-usb
-rsync -progress --modify-window=1 --update --recursive --times --stats --human-readable build/* /mnt/yoghi-usb 
-time umount /mnt/yoghi-usb 
+rsync -progress --modify-window=1 --update --recursive --times --stats --human-readable build/* /mnt/yoghi-usb
+time umount /mnt/yoghi-usb
 mount /mnt/yoghi-usb
 ls /mnt/yoghi-usb
+cp CDC-BitPrepared_USB2023.md5 /mnt/yoghi-usb
+cd /mnt/yoghi-usb
+md5sum -c CDC-BitPrepared_USB2023.md5
+rm CDC-BitPrepared_USB2023.md5
+cd
 umount /mnt/yoghi-usb
